5ea1db271c7714c18c0eeff713654e45b9ad569f
Gabriellsn <gabriellopo.sn@gmail.com>
2022-11-06 18:18:03 -0300

    primeira versao da brincadeira

---
+++ main.js
@@ -0,0 +1,39 @@
+function createGame(player1, hour, player2){
+    return`
+    <li>
+        <img src="./assets/icon=${player1}.svg" alt="bandeira ${player1}" />
+        <strong>${hour}</strong>
+        <img src="./assets/icon=${player2}.svg" alt="bandeira ${player2}" />
+    </li>
+   `
+
+
+
+}
+
+
+
+
+
+function createCard(date , day, games){
+    return `
+    <div class="card">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+        ${games}
+    </ul>
+    </div>
+    
+    `
+
+
+}
+
+
+document.querySelector("#cards").innerHTML = 
+
+    
+        createCard("24/11", "quinta",createGame('brazil', '16:00', 'serbia') ) +
+        createCard("28/11", "segunda",createGame('brazil', '13:00', 'Switzerland') + createGame('portugal', '16:00', 'uruguay')) +
+        createCard("02/12", "sexta",createGame('brazil', '16:00', 'cameroon')  )
+     ;
